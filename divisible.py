059f48fa346f51334f335cd4266b2d219a6b2c5a
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-12-03 10:20:01 +0530

    Divisible

---
+++ divisible.py
@@ -0,0 +1,6 @@
+a=int(input('Enter the number to be divided by'))
+b=int(input('Enter the number used to divide the first input'))
+if(a%b==0):
+    print(a,'is divisible by', b)
+else:
+    print(a, 'is not divisible by', b)
